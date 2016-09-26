54b0446dbe7a97a7ad368a6eb82ca92d66cd83c6
Kris Simon <kris@ausdertechnik.de>
2016-09-26 14:48:33 +0200

    Support for multible config files.

--- tasks/grunt-replace-configvalues.js
+++ tasks/grunt-replace-configvalues.js
@@ -17,7 +17,10 @@ module.exports = function (grunt) {
 		try {
 			replm = JSON.parse(replm)	
 		} catch(e) { replm = replm; }
-		cur[last] = replm;
+		if(_.has(cur, last)){
+			console.log("Replace "+ last +" with "+ replm);
+			cur[last] = replm;
+		}
 	}
 
     grunt.registerTask('configvalues', 'Replace config values.', function () {
@@ -28,27 +31,50 @@ module.exports = function (grunt) {
     
 		var confparam = _.compact(
 			_.map(process.argv, function(elm){
-				if(S(elm).startsWith("--conf")){
+				if(S(elm).startsWith("--conf:")){
 					return elm;
 				}
 			})
 		);
-		if(confparam.length > 0){
-			var buildconf = require(process.cwd()+"/"+options.src);
-			_.each(confparam, function(p){
-				var path = S(p).replace('--conf:', '').s;
-				var keyval = path.split("=");
-				console.log("Replace "+ keyval[0] +" with "+ keyval[1]);
-				selectPathAndReplaceWith(buildconf, keyval[0], keyval[1]);
-			});
-			fs.writeFile(options.src
-				, "module.exports = "+ JSON.stringify(buildconf, null, 4)
-				, function(err) {
-				    if(err) {
-				        grunt.error(err);
-				    }
+		
+		var conffiles = _.flatten(_.compact(
+			_.map(process.argv, function(elm){
+				if(S(elm).startsWith("--conffiles=")){
+					return elm.replace("--conffiles=", "").split(',');
 				}
-			);
+			})
+		));
+
+		if(conffiles.length <= 0){
+			conffiles = options.src
+		}
+		
+		if(confparam.length > 0){
+			if(typeof conffiles === 'string'){
+				conffiles = [conffiles];
+			}
+			_.each(conffiles, function(src){
+				var buildconf = require(process.cwd()+"/"+src);
+				_.each(confparam, function(p){
+					var path = S(p).replace('--conf:', '').s;
+					var keyval = path.split("=");
+					
+					selectPathAndReplaceWith(buildconf, keyval[0], keyval[1]);
+				});
+				var data = "";
+				if(S(src).endsWith(".js")){
+					data =  "module.exports = "+ JSON.stringify(buildconf, null, 4);
+				} else {
+					data =  JSON.stringify(buildconf, null, 4);
+				}
+				var err = fs.writeFileSync(
+					  src
+					, data
+				);
+				 if(err) {
+					grunt.error(err);
+				}				
+			});			
 		}
     })
 };
